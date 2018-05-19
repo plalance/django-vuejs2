85230c7878aa96b9844fa412abd94cefee185bf8
unknown <lalance.paul@gmail.com>
2018-05-19 15:47:30 +0200

    add gitignore

--- newapp/urls.py
+++ newapp/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls import url
 from journal.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
     url(r'^$', index, name='index'),
     url('toto', index, name='index'),
 ]
