3659e45aeb64db0e2ab28c1d7dbf439547474cce
unknown <lalance.paul@gmail.com>
2018-05-19 14:44:13 +0200

    Initial commit

---
+++ journal/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+
+    context = {
+        'days': [1, 2, 3],
+    }
+
+    return render(request, 'index.html', context)
+
